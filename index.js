3ca419f48ac74985273e0a54ca0019507b50ee86
Coders Code <magmaacademy@gmail.com>
2025-06-01 16:23:28 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",  // allow all origins
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
