22ad5b37b63023fff3bd74a47533a170bb430cb7
Coders Code <magmaacademy@gmail.com>
2025-06-02 23:36:30 +0500

    broadcast messages from VPS node app

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// // server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -8,22 +7,32 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",  // allow all origins
+        origin: "*", // Allow all origins (for testing)
         methods: ["GET", "POST"]
     }
 });
 
 io.on('connection', (socket) => {
     console.log('Client connected:', socket.id);
+
+    // Listen for a message from a client
+    socket.on('messageFromClient', (data) => {
+        console.log(`Message from ${socket.id}:`, data);
+
+        // Broadcast to all other sockets except the sender
+        socket.broadcast.emit('messageFromServer', data);
+
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconnected:', socket.id);
     });
 });
-//hello world how you are doing
 
 server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+    console.log('Server listening at http://localhost:3000');
 });
+
 
 process.on('SIGINT', () => {
     console.log('Shutting down server...');
@@ -33,4 +42,3 @@ process.on('SIGINT', () => {
         });
     });
 });
-
