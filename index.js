14f025840aa67358097a0e60c4c869a944880f1b
Coders Code <magmaacademy@gmail.com>
2025-06-02 19:25:59 +0500

    hello world

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-// server.js
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+// // server.js
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require('socket.io');
 
-const app = express();
-const server = http.createServer(app);
+// const app = express();
+// const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origin: "*",  // allow all origins
-        methods: ["GET", "POST"]
-    }
-});
+// const io = new Server(server, {
+//     cors: {
+//         origin: "*",  // allow all origins
+//         methods: ["GET", "POST"]
+//     }
+// });
 
-io.on('connection', (socket) => {
-    console.log('Client connected:', socket.id);
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
-    });
-});
-//hello world how you are doing
+// io.on('connection', (socket) => {
+//     console.log('Client connected:', socket.id);
+//     socket.on('disconnect', () => {
+//         console.log('Client disconnected:', socket.id);
+//     });
+// });
+// //hello world how you are doing
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
-});
+// server.listen(3000, () => {
+//     console.log('Server running at http://localhost:3000');
+// });
+
+console.log('hello world');
