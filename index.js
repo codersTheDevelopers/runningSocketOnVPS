8aeac7ad9d5844183d9a008997e7c3a2086e8c3d
Coders Code <magmaacademy@gmail.com>
2025-06-02 21:33:25 +0500

    kill process

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
 // // server.js
-// const express = require('express');
-// const http = require('http');
-// const { Server } = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-
-// const io = new Server(server, {
-//     cors: {
-//         origin: "*",  // allow all origins
-//         methods: ["GET", "POST"]
-//     }
-// });
-
-// io.on('connection', (socket) => {
-//     console.log('Client connected:', socket.id);
-//     socket.on('disconnect', () => {
-//         console.log('Client disconnected:', socket.id);
-//     });
-// });
-// //hello world how you are doing
-
-// server.listen(3000, () => {
-//     console.log('Server running at http://localhost:3000');
-// });
-
+const express = require('express');
 const http = require('http');
+const { Server } = require('socket.io');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n');
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",  // allow all origins
+        methods: ["GET", "POST"]
+    }
 });
 
-const PORT = 3000;
+io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+});
+//hello world how you are doing
 
-server.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}\nServer shut down after 2 minutes.`);
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
 
-    // Automatically close server after 2 minutes
-    setTimeout(() => {
+process.on('SIGINT', () => {
+    console.log('Shutting down server...');
+    io.close(() => {
         server.close(() => {
-            console.log('Server shut down after 2 minutes.');
+            process.exit(0);
         });
-    }, 2 * 60 * 1000); // 2 minutes
+    });
 });
+
